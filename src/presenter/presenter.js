f8c6a7382653f9874195f7a66b4468c2bf0374f8
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-24 17:26:20 +0700

    Исправлены замечания наставника

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,14 +1,22 @@
+import { render } from '../render.js';
+import ViewTripEventsList from '../view/view-trip-events-list-create.js';
 import ViewFormEdit from '../view/view-form-edit.js';
 import ViewTripPoint from '../view/view-trip-point.js';
-import { render } from '../render.js';
+import ViewFormCreate from '../view/view-form-edit.js';
 
-export default class Presenter{
-  init = (presenterContainer) => {
+export default class Presenter {
+  constructor(presenterContainer) {
     this.presenterContainer = presenterContainer;
+  }
+
+  init() {
     const TRIP_POINTS_COUNT = 3;
-    render(new ViewFormEdit(), this.presenterContainer);
+    const eventsList = new ViewTripEventsList();
+    render(eventsList, this.presenterContainer);
+    render(new ViewFormEdit(), eventsList.getElement());
     for (let i = 0; i < TRIP_POINTS_COUNT; i++) {
-      render(new ViewTripPoint(), this.presenterContainer);
+      render(new ViewTripPoint(), eventsList.getElement());
     }
-  };
+    render(new ViewFormCreate(), eventsList.getElement());
+  }
 }
