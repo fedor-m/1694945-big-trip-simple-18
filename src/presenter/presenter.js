63e410d17b99fdc55a0188ead4293b7ef9419c28
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-26 22:56:26 +0700

    Добавлена модель в конструктор

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,21 +1,42 @@
 import { render } from '../render.js';
 import ViewTripEventsList from '../view/view-trip-events-list-create.js';
 import ViewFormEdit from '../view/view-form-edit.js';
+import { generateDestination } from '../mock/destinations.js';
+import { generateOffers } from '../mock/offers.js';
 import ViewTripPoint from '../view/view-trip-point.js';
 import ViewFormCreate from '../view/view-form-create.js';
-const TRIP_POINTS_COUNT = 3;
+
+
 export default class Presenter {
-  constructor(presenterContainer) {
+  constructor(presenterContainer, model) {
     this.presenterContainer = presenterContainer;
+    this.model = model;
   }
 
   init() {
     const eventsList = new ViewTripEventsList();
+    const points = this.model.points;
+    const localPoint = {
+      model: this.model.localPoint,
+      destination: generateDestination(this.model.localPoint.destination),
+      offers: [],
+    };
     render(eventsList, this.presenterContainer);
-    render(new ViewFormEdit(), eventsList.getElement());
-    for (let i = 0; i < TRIP_POINTS_COUNT; i++) {
-      render(new ViewTripPoint(), eventsList.getElement());
+    for (const offer of this.model.localPoint.offers)
+    {
+      localPoint.offers.push(generateOffers(localPoint.type).offers.find((o)=>o.id === offer));
     }
-    render(new ViewFormCreate(), eventsList.getElement());
+    render(new ViewFormEdit(localPoint.model, localPoint.destination, localPoint.offers), eventsList.getElement());
+    for (const point of points) {
+      const destination = generateDestination(point.destination);
+      const offersList = generateOffers(point.type);
+      const offers = [];
+      for (const offer of point.offers)
+      {
+        offers.push((offersList.offers.find((o)=>o.id === offer)));
+      }
+      render(new ViewTripPoint(point, destination, offers), eventsList.getElement());
+    }
+    render(new ViewFormCreate(localPoint.model, localPoint.destination, localPoint.offers), eventsList.getElement());
   }
 }
