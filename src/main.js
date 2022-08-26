d33eb9f9995fe1ec21779c4d458535b12d67b164
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-26 22:57:30 +0700

    Импортирована модель с последующим созданием её экземпляра

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import ViewFilters from './view/view-filters.js';
 import ViewSort from './view/view-sort.js';
 import { render } from './render.js';
+import Model from './model/model.js';
 import Presenter from './presenter/presenter.js';
 
 const tripFiltersSection = document.querySelector('.trip-controls__filters');
 const tripEventsSection = document.querySelector('.trip-events');
-const presenter = new Presenter(tripEventsSection);
+const model = new Model();
+const presenter = new Presenter(tripEventsSection, model);
 
 render(new ViewFilters(), tripFiltersSection);
 render(new ViewSort(), tripEventsSection);
