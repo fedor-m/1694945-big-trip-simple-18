5c8eb0e5f1600ac800842c5c30ae3ff42a50564b
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-28 18:31:52 +0700

    Переписана функция, генерирующая точку

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,14 +1,16 @@
-import { getRandomInteger, getRandomArrayElement, getRandomArray } from '../utils.js';
+import {
+  getRandomInteger,
+  getRandomArrayElement
+} from '../utils.js';
 import { TYPES } from './types.js';
 import { DESTINATIONS } from './destinations.js';
-import { OFFERS } from './offers.js';
-const checkArray = (arr) => arr.includes(0) ? [0] : arr;
+import { generateOffers } from './offers.js';
 export const generatePoint = () => ({
   basePrice: getRandomInteger(1000, 2000),
   dateFrom: new Date(Date.now()).toISOString(),
-  dateTo: new Date(Date.now() + (3600 * 1000 * 24) + (3600 * 1000)).toISOString(),
+  dateTo: new Date(Date.now() + 3600 * 1000 * 24 + 3600 * 1000).toISOString(),
   destination: getRandomArrayElement(DESTINATIONS).id,
   id: getRandomInteger(1, 100),
-  offers: checkArray(getRandomArray(OFFERS).map((o) => o.id)),
-  type: getRandomArrayElement(TYPES)
+  offers: generateOffers(),
+  type: getRandomArrayElement(TYPES),
 });
