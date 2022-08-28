f5a260d92e02924247dc41d3e324c5331de9b597
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-28 20:15:22 +0700

    Переписаны функции генерации разметки списков

--- src/view/view-form-create.js
+++ src/view/view-form-create.js
@@ -7,37 +7,26 @@ const createFormCreateTemplate = (point, destination, offers) => {
   const { type: pointType, dateFrom, dateTo, basePrice } = point;
   const { name, description, pictures } = destination;
   const { offers: selectedOffers } = offers;
-  let listTypes = '';
-  let listDestinations = '';
-  let listOffers = '';
-  let listPictures = '';
   const allOffers = OFFERS.filter((offer)=>offer.id !== 0);
-  for (const type of TYPES) {
-    listTypes += (`
-      <div class="event__type-item">
-        <input id="event-type-${ type }-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${ type }" ${ type === pointType ? 'checked=""' : '' }>
-        <label class="event__type-label  event__type-label--${ type }" for="event-type-${ type }-1">${ type[0].toUpperCase() }${ type.slice(1) }</label>
-      </div>`);
-  }
-  for (const destinationItem of DESTINATIONS) {
-    listDestinations += (`<option value="${ destinationItem.name} "></option>`);
-  }
-  for (const picture of pictures) {
-    listPictures += (`<img class="event__photo" src="${ picture.src }" alt="Event photo"></img>`);
-  }
-  for (const offer of allOffers)
-  {
+  const listTypesMarkup = TYPES.map((type)=>(`
+  <div class="event__type-item">
+    <input id="event-type-${ type }-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${ type }" ${ type === pointType ? 'checked=""' : '' }>
+    <label class="event__type-label  event__type-label--${ type }" for="event-type-${ type }-1">${ type[0].toUpperCase() }${ type.slice(1) }</label>
+  </div>`)).join('');
+  const listDestinationsMarkup = DESTINATIONS.map((destinationItem)=>(`<option value="${ destinationItem.name} "></option>`)).join('');
+  const listOffersMarkup = allOffers.map((offer)=>{
     const selected = (selectedOffers.find((selectedOffer) => selectedOffer.id === offer.id)) ? 'checked=""' : '';
-    listOffers += (`
-      <div class="event__offer-selector">
-        <input class="event__offer-checkbox  visually-hidden" id="event-${ offer.title.replaceAll(' ', '-').toLowerCase() }-1" type="checkbox" name="${ offer.title.replaceAll (' ', '-').toLowerCase()}" ${ selected }>
-        <label class="event__offer-label" for="${ offer.title.replaceAll(' ', '-').toLowerCase () }-1">
-          <span class="event__offer-title">${ offer.title }</span>
-          +€&nbsp;
-          <span class="event__offer-price">${ offer.price }</span>
-        </label>
-      </div>`);
-  }
+    return (`
+    <div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="event-${ offer.title.replaceAll(' ', '-').toLowerCase() }-1" type="checkbox" name="${ offer.title.replaceAll (' ', '-').toLowerCase()}" ${ selected }>
+      <label class="event__offer-label" for="${ offer.title.replaceAll(' ', '-').toLowerCase () }-1">
+        <span class="event__offer-title">${ offer.title }</span>
+        +€&nbsp;
+        <span class="event__offer-price">${ offer.price }</span>
+      </label>
+    </div>`);
+  }).join('');
+  const listPicturesMarkup = pictures.map((picture)=>(`<img class="event__photo" src="${ picture.src }" alt="Event photo"></img>`)).join();
   return `
   <li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
@@ -51,7 +40,7 @@ const createFormCreateTemplate = (point, destination, offers) => {
           <div class="event__type-list">
             <fieldset class="event__type-group">
               <legend class="visually-hidden">Event type</legend>
-              ${ listTypes }
+              ${ listTypesMarkup }
             </fieldset>
           </div>
         </div>
@@ -62,24 +51,24 @@ const createFormCreateTemplate = (point, destination, offers) => {
           <input class="event__input  event__input--destination" id="event-destination-1"     type="text"
             name="event-destination" value="${ name }" list="destination-list-1">
           <datalist id="destination-list-1">
-            ${ listDestinations }
+            ${ listDestinationsMarkup }
           </datalist>
         </div>
         <div class="event__field-group  event__field-group--time">
           <label class="visually-hidden" for="event-start-time-1">From</label>
           <input class="event__input  event__input--time" id="event-start-time-1"   type="text"   name="event-start-time"
-            value="${dayjs(dateFrom).format('DD/MM/YY HH:MM')}">
+            value="${ dayjs(dateFrom).format('DD/MM/YY HH:MM') }">
           —
           <label class="visually-hidden" for="event-end-time-1">To</label>
           <input class="event__input  event__input--time" id="event-end-time-1"   type="text"   name="event-end-time"
-            value="${dayjs(dateTo).format('DD/MM/YY HH:MM')}">
+            value="${ dayjs(dateTo).format('DD/MM/YY HH:MM') }">
         </div>
         <div class="event__field-group  event__field-group--price">
           <label class="event__label" for="event-price-1">
             <span class="visually-hidden">Price</span>
             €
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text"     name="event-price" value="${basePrice}">
+          <input class="event__input  event__input--price" id="event-price-1" type="text"     name="event-price" value="${ basePrice }">
         </div>
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
         <button class="event__reset-btn" type="reset">Cancel</button>
@@ -88,7 +77,7 @@ const createFormCreateTemplate = (point, destination, offers) => {
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
           <div class="event__available-offers">
-            ${ listOffers }
+            ${ listOffersMarkup }
           </div>
         </section>
         <section class="event__section  event__section--destination">
@@ -96,7 +85,7 @@ const createFormCreateTemplate = (point, destination, offers) => {
           <p class="event__destination-description">${ description }</p>
           <div class="event__photos-container">
             <div class="event__photos-tape">
-              ${ listPictures }
+              ${ listPicturesMarkup }
             </div>
           </div>
         </section>
--- src/view/view-form-edit.js
+++ src/view/view-form-edit.js
@@ -7,33 +7,25 @@ const createFormEditTemplate = (point, destination, offers) => {
   const { type: pointType, dateFrom, dateTo, basePrice } = point;
   const { name, description } = destination;
   const { offers: selectedOffers } = offers;
-  let listTypes = '';
-  let listDestinations = '';
-  let listOffers = '';
   const allOffers = OFFERS.filter((offer)=>offer.id !== 0);
-  for (const type of TYPES) {
-    listTypes += (`
-      <div class="event__type-item">
-        <input id="event-type-${ type }-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${ type }" ${ type === pointType ? 'checked=""' : '' }>
-        <label class="event__type-label  event__type-label--${ type }" for="event-type-${ type }-1">${ type[0].toUpperCase() }${ type.slice(1) }</label>
-      </div>`);
-  }
-  for (const destinationItem of DESTINATIONS) {
-    listDestinations += (`<option value="${ destinationItem.name} "></option>`);
-  }
-  for (const offer of allOffers)
-  {
-    const selected = (selectedOffers.find(( selectedOffer ) => selectedOffer.id === offer.id)) ? 'checked=""' : '';
-    listOffers += (`
-      <div class="event__offer-selector">
-        <input class="event__offer-checkbox  visually-hidden" id="event-${ offer.title.replaceAll(' ', '-').toLowerCase() }-1" type="checkbox" name="${ offer.title.replaceAll (' ', '-').toLowerCase()}" ${ selected }>
-        <label class="event__offer-label" for="${ offer.title.replaceAll(' ', '-').toLowerCase () }-1">
-          <span class="event__offer-title">${ offer.title }</span>
-          +€&nbsp;
-          <span class="event__offer-price">${ offer.price }</span>
-        </label>
-      </div>`);
-  }
+  const listTypesMarkup = TYPES.map((type)=>(`
+  <div class="event__type-item">
+    <input id="event-type-${ type }-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${ type }" ${ type === pointType ? 'checked=""' : '' }>
+    <label class="event__type-label  event__type-label--${ type }" for="event-type-${ type }-1">${ type[0].toUpperCase() }${ type.slice(1) }</label>
+  </div>`)).join('');
+  const listDestinationsMarkup = DESTINATIONS.map((destinationItem)=>(`<option value="${ destinationItem.name} "></option>`)).join('');
+  const listOffersMarkup = allOffers.map((offer)=>{
+    const selected = (selectedOffers.find((selectedOffer) => selectedOffer.id === offer.id)) ? 'checked=""' : '';
+    return (`
+    <div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="event-${ offer.title.replaceAll(' ', '-').toLowerCase() }-1" type="checkbox" name="${ offer.title.replaceAll (' ', '-').toLowerCase()}" ${ selected }>
+      <label class="event__offer-label" for="${ offer.title.replaceAll(' ', '-').toLowerCase () }-1">
+        <span class="event__offer-title">${ offer.title }</span>
+        +€&nbsp;
+        <span class="event__offer-price">${ offer.price }</span>
+      </label>
+    </div>`);
+  }).join('');
   return `
   <li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
@@ -47,7 +39,7 @@ const createFormEditTemplate = (point, destination, offers) => {
           <div class="event__type-list">
             <fieldset class="event__type-group">
               <legend class="visually-hidden">Event type</legend>
-              ${ listTypes }
+              ${ listTypesMarkup }
             </fieldset>
           </div>
         </div>
@@ -58,24 +50,24 @@ const createFormEditTemplate = (point, destination, offers) => {
           <input class="event__input  event__input--destination" id="event-destination-1"     type="text"
             name="event-destination" value="${ name }" list="destination-list-1">
           <datalist id="destination-list-1">
-            ${ listDestinations }
+            ${ listDestinationsMarkup }
           </datalist>
         </div>
         <div class="event__field-group  event__field-group--time">
           <label class="visually-hidden" for="event-start-time-1">From</label>
           <input class="event__input  event__input--time" id="event-start-time-1"   type="text"   name="event-start-time"
-            value="${dayjs(dateFrom).format('DD/MM/YY HH:MM')}">
+            value="${ dayjs(dateFrom).format('DD/MM/YY HH:MM') }">
           —
           <label class="visually-hidden" for="event-end-time-1">To</label>
           <input class="event__input  event__input--time" id="event-end-time-1"   type="text"   name="event-end-time"
-            value="${dayjs(dateTo).format('DD/MM/YY HH:MM')}">
+            value="${ dayjs(dateTo).format('DD/MM/YY HH:MM') }">
         </div>
         <div class="event__field-group  event__field-group--price">
           <label class="event__label" for="event-price-1">
             <span class="visually-hidden">Price</span>
             €
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text"     name="event-price" value="${basePrice}">
+          <input class="event__input  event__input--price" id="event-price-1" type="text"     name="event-price" value="${ basePrice }">
         </div>
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
         <button class="event__reset-btn" type="reset">Cancel</button>
@@ -84,7 +76,7 @@ const createFormEditTemplate = (point, destination, offers) => {
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
           <div class="event__available-offers">
-            ${ listOffers }
+            ${ listOffersMarkup }
           </div>
         </section>
         <section class="event__section  event__section--destination">
