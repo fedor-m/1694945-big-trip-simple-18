cffabc9e335459053b0eee66777da03dbe2c5fcc
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-31 15:48:48 +0700

    Переименована модель в точке входа

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import ViewFilters from './view/view-filters.js';
 import ViewSort from './view/view-sort.js';
 import { render } from './render.js';
-import ModelPoints from './model/model-points.js';
+import PointsModel from './model/points-model.js';
 import EventsPresenter from './presenter/events-presenter';
 
 const TRIP_POINTS_COUNT = 3;
 const tripFiltersSection = document.querySelector('.trip-controls__filters');
 const tripEventsSection = document.querySelector('.trip-events');
-const modelPoints = new ModelPoints(TRIP_POINTS_COUNT);
-const presenter = new EventsPresenter(tripEventsSection, modelPoints);
+const pointsModel = new PointsModel(TRIP_POINTS_COUNT);
+const presenter = new EventsPresenter(tripEventsSection, pointsModel);
 
 render(new ViewFilters(), tripFiltersSection);
 render(new ViewSort(), tripEventsSection);
