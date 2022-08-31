1e8e4ebbade69e6cfdfdd3581769477943a048d4
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-08-31 15:41:33 +0700

    Переименован презентер в коде

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import ViewFilters from './view/view-filters.js';
 import ViewSort from './view/view-sort.js';
 import { render } from './render.js';
 import ModelPoints from './model/model-points.js';
-import PresenterEvents from './presenter/presenter-events.js';
+import EventsPresenter from './presenter/events-presenter';
 
 const TRIP_POINTS_COUNT = 3;
 const tripFiltersSection = document.querySelector('.trip-controls__filters');
 const tripEventsSection = document.querySelector('.trip-events');
 const modelPoints = new ModelPoints(TRIP_POINTS_COUNT);
-const presenter = new PresenterEvents(tripEventsSection, modelPoints);
+const presenter = new EventsPresenter(tripEventsSection, modelPoints);
 
 render(new ViewFilters(), tripFiltersSection);
 render(new ViewSort(), tripEventsSection);
