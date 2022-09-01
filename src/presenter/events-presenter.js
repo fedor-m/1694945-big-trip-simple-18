5de7691da6a792bcef9029e363c7479de15b0ee3
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-01 17:26:27 +0700

    Добавлена функция '#renderTask'

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,53 +1,79 @@
 import { render } from '../render.js';
 import ViewTripEventsList from '../view/view-trip-events-list-create.js';
-import ViewFormEdit from '../view/view-form-edit.js';
 import { generateDestination } from '../mock/destinations.js';
 import { generateOffersByType } from '../mock/offers.js';
 import ViewTripPoint from '../view/view-trip-point.js';
-import ViewFormCreate from '../view/view-form-create.js';
+import ViewFormEdit from '../view/view-form-edit.js';
 
 export default class EventsPresenter {
+  #presenterContainer = null;
+  #model = null;
+  #eventsList = new ViewTripEventsList();
+
   constructor(presenterContainer, model) {
-    this.presenterContainer = presenterContainer;
-    this.model = model;
+    this.#presenterContainer = presenterContainer;
+    this.#model = model;
   }
 
   init() {
-    const eventsList = new ViewTripEventsList();
-    const points = this.model.points;
-    const localPointModel = this.model.localPoint;
-    const localPoint = {
-      model: localPointModel,
-      destination: generateDestination(localPointModel.destination),
-      offers: generateOffersByType(
-        localPointModel.type,
-        localPointModel.offers,
-      ),
-    };
-    render(eventsList, this.presenterContainer);
-    render(
-      new ViewFormEdit(
-        localPoint.model,
-        localPoint.destination,
-        localPoint.offers,
-      ),
-      eventsList.element,
-    );
+    const points = this.#model.points;
+    render(this.#eventsList, this.#presenterContainer);
     for (const point of points) {
       const destination = generateDestination(point.destination);
       const offers = generateOffersByType(point.type, point.offers);
-      render(
-        new ViewTripPoint(point, destination, offers),
-        eventsList.element,
-      );
+      this.#renderPoint(point, destination, offers);
     }
-    render(
-      new ViewFormCreate(
-        localPoint.model,
-        localPoint.destination,
-        localPoint.offers,
-      ),
-      eventsList.element,
-    );
   }
+
+  #renderPoint = (point, destination, offers) => {
+    const pointComponent = new ViewTripPoint(point, destination, offers);
+    const pointEditComponent = new ViewFormEdit(point, destination, offers);
+    const replacePointToForm = () => {
+      this.#eventsList.element.replaceChild(
+        pointEditComponent.element,
+        pointComponent.element,
+      );
+    };
+
+    const replaceFormToPoint = () => {
+      this.#eventsList.element.replaceChild(
+        pointComponent.element,
+        pointEditComponent.element,
+      );
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    const onFormReplace = () =>{
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    };
+
+    pointComponent.element
+      .querySelector('.event__rollup-btn')
+      .addEventListener('click', () => {
+        replacePointToForm();
+        document.addEventListener('keydown', onEscKeyDown);
+      });
+
+    pointEditComponent.element
+      .querySelector('form')
+      .addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        onFormReplace();
+      });
+
+    pointEditComponent.element
+      .querySelector('.event__rollup-btn')
+      .addEventListener('click', () => {
+        onFormReplace();
+      });
+    render(pointComponent, this.#eventsList.element);
+  };
 }
