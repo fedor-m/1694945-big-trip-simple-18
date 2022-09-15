c3469d32f0f7e0a5a2350cff514415ebccafa684
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-15 14:00:32 +0700

    Вынесен код отрисовки точки, внесён код отрисовки кнопок для сортировки

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,11 +1,12 @@
-import { render, replace } from '../framework/render.js';
+import { render } from '../framework/render.js';
 import ViewTripEventsList from '../view/view-trip-events-list-create.js';
 import { generateDestination } from '../mock/destinations.js';
 import { generateOffersByType } from '../mock/offers.js';
 import ViewNoEvents from '../view/view-no-events.js';
-import ViewTripPoint from '../view/view-trip-point.js';
-import ViewFormEdit from '../view/view-form-edit.js';
-import { isEscKey } from '../utils.js';
+import SortTypesModel from '../model/sort-types-model.js';
+import { SORT_TYPES } from '../mock/sort.js';
+import SortPresenter from './sort-presenter.js';
+import PointPresenter from './point-presenter.js';
 
 export default class EventsPresenter {
   #presenterContainer;
@@ -16,58 +17,25 @@ export default class EventsPresenter {
     this.#model = model;
   }
 
-  init() {
+  init = () => {
     const points = this.#model.points;
     if (points.length === 0) {
       render(new ViewNoEvents(), this.#presenterContainer);
       return;
     }
     render(this.#eventsList, this.#presenterContainer);
+    const sortTypesModel = new SortTypesModel(SORT_TYPES);
+    const sortPresenter = new SortPresenter(this.#eventsList.element, sortTypesModel);
+    sortPresenter.init();
     for (const point of points) {
       const destination = generateDestination(point.destination);
       const offers = generateOffersByType(point.type, point.offers);
       this.#renderPoint(point, destination, offers);
     }
-  }
+  };
 
   #renderPoint = (point, destination, offers) => {
-    const pointComponent = new ViewTripPoint(point, destination, offers);
-    const pointEditComponent = new ViewFormEdit(point, destination, offers);
-
-    const replacePointToForm = () => {
-      replace(pointEditComponent, pointComponent);
-    };
-
-    const replaceFormToPoint = () => {
-      replace(pointComponent, pointEditComponent);
-    };
-
-    const onEscKeyDown = (evt) => {
-      if (isEscKey(evt.key)) {
-        evt.preventDefault();
-        replaceFormToPoint();
-        document.removeEventListener('keydown', onEscKeyDown);
-      }
-    };
-
-    const onFormReplace = () => {
-      replaceFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    };
-
-    pointComponent.setEditClickHandler(() => {
-      replacePointToForm();
-      document.addEventListener('keydown', onEscKeyDown);
-    });
-
-    pointEditComponent.setFormSubmitHandler(() => {
-      onFormReplace();
-    });
-
-    pointEditComponent.setEditClickHandler(() => {
-      onFormReplace();
-    });
-
-    render(pointComponent, this.#eventsList.element);
+    const eventPresenter = new PointPresenter(this.#eventsList.element);
+    eventPresenter.init(point, destination, offers);
   };
 }
