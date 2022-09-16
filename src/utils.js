c676961948075c98510ee07233ef5821bc5f9049
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-16 10:19:35 +0700

    Добавлена функция 'updateItem'

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,17 @@ export const shuffleArray = (elements) => {
   return copiedElements;
 };
 export const isEscKey = (key) => ESCAPE_CODE.indexOf(key) > -1;
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
