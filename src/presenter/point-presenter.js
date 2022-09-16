76d9642a7bcd66f9aa5c9475a1ba94f7b4d6bfaf
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-16 10:21:08 +0700

    Добавлены функции вызова из обработчиков

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,4 +1,4 @@
-import { render, replace } from '../framework/render.js';
+import { render, replace, remove } from '../framework/render.js';
 import ViewTripPoint from '../view/view-trip-point.js';
 import ViewFormEdit from '../view/view-form-edit.js';
 import { isEscKey } from '../utils.js';
@@ -18,42 +18,74 @@ export default class PointPresenter {
     this.#point = point;
     this.#destination = destination;
     this.#offers = offers;
-    this.#pointComponent = new ViewTripPoint(point, destination, offers);
-    this.#pointEditComponent = new ViewFormEdit(point, destination, offers);
-    this.#pointComponent.setEditClickHandler(() => {
-      this.#replacePointToForm();
-      document.addEventListener('keydown', this.#onEscKeyDown);
-    });
 
-    this.#pointEditComponent.setFormSubmitHandler(() => {
-      this.#onFormReplace();
-    });
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#pointEditComponent;
 
-    this.#pointEditComponent.setEditClickHandler(() => {
-      this.#onFormReplace();
-    });
 
-    render(this.#pointComponent, this.#eventsList);
+    this.#pointComponent = new ViewTripPoint(this.#point, this.#destination, this.#offers);
+    this.#pointEditComponent = new ViewFormEdit(this.#point, this.#destination, this.#offers);
+
+    this.#pointComponent.setEditClickHandler(this.#handleEditClick);
+    this.#pointEditComponent.setFormClickHandler(this.#handleFormClick);
+    this.#pointEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
+    this.#pointEditComponent.setFormResetHandler(this.#handleFormReset);
+
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this.#pointComponent, this.#eventsList);
+      return;
+    }
+
+    // Проверка на наличие в DOM необходима,
+    // чтобы не пытаться заменить то, что не было отрисовано
+    if (this.#eventsList.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#eventsList.contains(prevPointEditComponent.element)) {
+      replace(this.#pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  };
+
+  destroy = () => {
+    remove(this.#pointComponent);
+    remove(this.#pointEditComponent);
   };
 
   #replacePointToForm = () => {
     replace(this.#pointEditComponent, this.#pointComponent);
+    document.addEventListener('keydown', this.#onEscKeyDown);
   };
 
   #replaceFormToPoint = () => {
     replace(this.#pointComponent, this.#pointEditComponent);
+    document.removeEventListener('keydown', this.#onEscKeyDown);
   };
 
   #onEscKeyDown = (evt) => {
     if (isEscKey(evt.key)) {
       evt.preventDefault();
       this.#replaceFormToPoint();
-      document.removeEventListener('keydown', this.#onEscKeyDown);
     }
   };
 
-  #onFormReplace = () => {
+  #handleEditClick = () => {
+    this.#replacePointToForm();
+  };
+
+  #handleFormClick = () => {
     this.#replaceFormToPoint();
-    document.removeEventListener('keydown', this.#onEscKeyDown);
+  };
+
+  #handleFormSubmit = () => {
+    this.#replaceFormToPoint();
+  };
+
+  #handleFormReset = () => {
+    this.#replaceFormToPoint();
   };
 }
+
