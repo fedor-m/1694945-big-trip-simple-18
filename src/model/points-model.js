c14a464d354ea3939f7e90427321cfc11ec5557b
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-27 09:04:14 +0700

    Модель точек наследуется от класса 'Observable'

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,12 +1,13 @@
+import Observable from '../framework/observable.js';
+import { getRandomInteger } from '../utils/common.js';
 import { generatePoint } from '../mock/point.js';
-export default class PointsModel {
-  #length = null;
-  constructor(length) {
-    this.#length = length;
-  }
+const MIN_POINTS = 0;
+const MAX_POINTS = 10;
+const tripPointsCount = getRandomInteger(MIN_POINTS, MAX_POINTS);
+export default class PointsModel extends Observable {
 
   get points() {
-    return Array.from({ length: this.#length }, generatePoint);
+    return Array.from({ length: tripPointsCount }, generatePoint);
   }
 
   get localPoint() {
