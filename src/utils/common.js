71e44be4288c1683f66574d92f50eb4cc1f7bc19
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-27 10:29:12 +0700

    Удалена функция 'updateItem'

--- src/utils/common.js
+++ src/utils/common.js
@@ -24,17 +24,6 @@ export const shuffleArray = (elements) => {
   }
   return copiedElements;
 };
-export const updateItem = (items, update) => {
-  const index = items.findIndex((item) => item.id === update.id);
-  if (index === -1) {
-    return items;
-  }
-  return [
-    ...items.slice(0, index),
-    update,
-    ...items.slice(index + 1),
-  ];
-};
 const getRandomRgbColor = () => {
   const r = getRandomInteger(0, 255);
   const g = getRandomInteger(0, 255);
