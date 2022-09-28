b42fbc61b75ce869b96c8f679cf5867ae172a656
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-28 10:27:47 +0700

    Добавлены/изменены константы для типов фильтров

--- src/mock/filters.js
+++ src/mock/filters.js
@@ -1,2 +1,8 @@
-export const FILTERS = ['everything', 'future'];
-export const filterEnabledOnNoEvents = 'everything';
+export const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+};
+export const NoEventsTextType = {
+  [FilterType.EVERYTHING]: 'Click New Event to create your first point',
+  [FilterType.FUTURE]: 'There are no future events now',
+};
