022a1954c0a4c4c2a10d4fbdc20e28cdc27bdd79
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-28 19:00:07 +0700

    Добавлены константы для типов формы

---
+++ src/const/form.js
@@ -0,0 +1,8 @@
+export const ViewFormType = {
+  ADD_FORM: 'add',
+  EDIT_FORM: 'edit',
+};
+export const ViewFormTypeButton = {
+  [ViewFormType.ADD_FORM]: 'Cancel',
+  [ViewFormType.EDIT_FORM]: 'Delete',
+};
