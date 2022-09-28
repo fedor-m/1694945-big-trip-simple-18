240c930892985f32624a08654bc6a2cacbe21416
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-28 14:25:26 +0700

    Возвращена структура 'LocalPoint'

---
+++ src/mock/local-point.js
@@ -0,0 +1,15 @@
+import { getRandomInteger } from '../utils/common.js';
+import { nanoid } from 'nanoid';
+import { getRandomDestinationID } from './destinations.js';
+import { getRandomType } from './types.js';
+const MIN_PRICE = 1000;
+const MAX_PRICE = 2000;
+export const generateLocalPoint = () => ({
+  basePrice: getRandomInteger(MIN_PRICE, MAX_PRICE),
+  dateFrom: new Date().toISOString(),
+  dateTo: new Date().toISOString(),
+  destination: getRandomDestinationID(),
+  id: nanoid(),
+  offers: [],
+  type: getRandomType(),
+});
