325552fae26ba2f0d3ef30bad0b1464cc5816bb1
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-28 12:55:11 +0700

    Произведён рефакторинг

--- src/mock/sort.js
+++ src/mock/sort.js
@@ -2,8 +2,7 @@ export const SORT_TYPES = [
   'day','event', 'time', 'price', 'offers'
 ];
 export const SortType = {
-  DEFAULT: 'default',
   DAY: 'day',
   PRICE: 'price',
 };
-export const SORT_TYPE_DEFAULT = 'day';
+export const SORT_TYPE_DEFAULT = SortType.DAY;
