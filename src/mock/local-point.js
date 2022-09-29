5436b5bae306746c50cb56d1af7703d76186df82
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-29 09:33:36 +0700

    Удалены некоторые параметры

--- src/mock/local-point.js
+++ src/mock/local-point.js
@@ -1,15 +1,11 @@
-import { getRandomInteger } from '../utils/common.js';
-import { nanoid } from 'nanoid';
 import { getRandomDestinationID } from './destinations.js';
 import { getRandomType } from '../const/types.js';
-const MIN_PRICE = 1000;
-const MAX_PRICE = 2000;
+import { MIN_PRICE } from '../const/form.js';
 export const generateLocalPoint = () => ({
-  basePrice: getRandomInteger(MIN_PRICE, MAX_PRICE),
+  basePrice: MIN_PRICE,
   dateFrom: new Date().toISOString(),
   dateTo: new Date().toISOString(),
   destination: getRandomDestinationID(),
-  id: nanoid(),
   offers: [],
   type: getRandomType(),
 });
