a362e008bd32442af479e57c93d97485c5e1fc22
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-09-30 11:59:14 +0700

    Добавлена инициализация получения списка точек с сервера

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -7,8 +7,20 @@ const MIN_POINTS = 0;
 const MAX_POINTS = 10;
 const tripPointsCount = getRandomInteger(MIN_POINTS, MAX_POINTS);
 export default class PointsModel extends Observable {
-  #points = Array.from({ length: tripPointsCount }, generatePoint).sort(sortByDay);
+  #pointsApiService = null;
+  #points = Array.from({ length: tripPointsCount }, generatePoint).sort(
+    sortByDay,
+  );
+
   #localPoint = generateLocalPoint();
+
+  constructor(pointsApiService) {
+    super();
+    this.#pointsApiService = pointsApiService;
+    this.#pointsApiService.points.then((points) => {
+      console.log(points);
+    });
+  }
 
   get points() {
     return this.#points;
@@ -20,26 +32,19 @@ export default class PointsModel extends Observable {
 
   updatePoint = (updateType, update) => {
     const index = this.#points.findIndex((point) => point.id === update.id);
-
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
-
     this.#points = [
       ...this.#points.slice(0, index),
       update,
       ...this.#points.slice(index + 1),
     ];
-
     this._notify(updateType, update);
   };
 
   addPoint = (updateType, update) => {
-    this.#points = [
-      update,
-      ...this.#points,
-    ];
-
+    this.#points = [update, ...this.#points];
     this._notify(updateType, update);
   };
 
@@ -48,12 +53,10 @@ export default class PointsModel extends Observable {
     if (index === -1) {
       throw new Error('Can\'t delete unexisting point');
     }
-
     this.#points = [
       ...this.#points.slice(0, index),
       ...this.#points.slice(index + 1),
     ];
-
     this._notify(updateType);
   };
 }
