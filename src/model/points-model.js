700d2c4cff22e1410a844465d75e8d3f70070c8b
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-10-01 18:04:57 +0700

    Добавлены/изменены методы с обращением к серверу

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -37,34 +37,49 @@ export default class PointsModel extends Observable {
     return this.#offers;
   }
 
-  updatePoint = (updateType, update) => {
+  addPoint = async (updateType, update) => {
+    try {
+      const response = await this.#pointsApiService.addPoint(update);
+      const newPoint = this.#adaptToClient(response);
+
+      this.#points = [
+        ...this.#points,
+        update
+      ];
+      this._notify(updateType, newPoint);
+    } catch(err) {
+      throw new Error('Can\'t add point');
+    }
+
+  };
+
+  updatePoint = async (updateType, update) => {
     const index = this.#points.findIndex((point) => point.id === update.id);
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
-    this.#points = [
-      ...this.#points.slice(0, index),
-      update,
-      ...this.#points.slice(index + 1),
-    ];
-    this._notify(updateType, update);
-  };
-
-  addPoint = (updateType, update) => {
-    this.#points = [update, ...this.#points];
-    this._notify(updateType, update);
-  };
-
-  deletePoint = (updateType, update) => {
-    const index = this.#points.findIndex((point) => point.id === update.id);
-    if (index === -1) {
-      throw new Error('Can\'t delete unexisting point');
+    try {
+      const response = await this.#pointsApiService.updatePoint(update);
+      const updatedPoint = this.#adaptToClient(response);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatedPoint,
+        ...this.#points.slice(index + 1)
+      ];
+      this._notify(updateType, updatedPoint);
+    } catch(err) {
+      throw new Error('Can\'t update point');
     }
-    this.#points = [
-      ...this.#points.slice(0, index),
-      ...this.#points.slice(index + 1),
-    ];
-    this._notify(updateType);
+  };
+
+  deletePoint = async (updateType, update) => {
+    try {
+      await this.#pointsApiService.deletePoint(update);
+      this.#points = this.#points.filter((point) => point.id !== update.id);
+      this._notify(updateType, update);
+    } catch(err) {
+      throw new Error('Can\'t delete point');
+    }
   };
 
   #adaptToClient = (point) => {
